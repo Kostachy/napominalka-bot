cfd6a57b4048f2d56d0ca3f535117d86f23e21bf
Kostachy <kostachy@gmail.com>
2023-11-30 01:18:41 +0300

    add some tests

--- db/database.py
+++ db/database.py
@@ -13,7 +13,8 @@ from sqlalchemy.orm import DeclarativeBase
 
 from config.bot_config import config
 
-DATABASE_URL = f"postgresql+asyncpg://{config.postgres.db_user}:{config.postgres.db_pass}@{config.postgres.db_host}:{config.postgres.db_port}/{config.postgres.db_name}"
+
+DATABASE_URL = f"postgresql+asyncpg://{config.postgres.db_user}:{config.postgres.db_pass}@{config.postgres.db_host}:{config.postgres.db_port}/napominalka_db_test"
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
--- tests/test_db/test_crud.py
+++ tests/test_db/test_crud.py
@@ -3,14 +3,12 @@ import pytest
 from db.crud import UserCRUD
 
 
-class TestUserCRUD:
-    @pytest.mark.parametrize(
-        "user_id",
-        [
-            1,
-            2,
-            3,
-        ]
-    )
-    def test_user_crud(self, user_id: int):
-        assert UserCRUD.read_user(user_id=user_id)
+@pytest.mark.parametrize(
+    "user_id",
+    [
+        1,
+    ]
+)
+async def test_user_crud(user_id: int):
+    user = await UserCRUD.read_user(user_id=user_id)
+    assert user
